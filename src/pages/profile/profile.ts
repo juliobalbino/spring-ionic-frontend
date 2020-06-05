92ac11601bd8be2e61a622f2a5ff191644809d19
Julio Balbino <julio4110@hotmail.com>
2020-06-05 15:07:09 -0300

    Buscando enderecos do banco de dados

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -26,7 +26,7 @@ export class ProfilePage {
     if (localStorage && localUser.email) {
       this.clienteService.findByEmail(localUser.email)
         .subscribe(response => {
-          this.cliente = response;
+          this.cliente = response as ClienteDTO;
           this.getImageIfExists();
         },
         error => {
