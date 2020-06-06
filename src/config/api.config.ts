f722ea4a236058045c02fa3ff7ec814eea35398e
Julio Balbino <julio4110@hotmail.com>
2020-06-06 19:11:43 -0300

    Testando o app no dispositivo

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,4 @@
 export const API_CONFIG = {
-    baseUrl: "http://localhost:8080",
+    baseUrl: "https://jbalbino-curso-spring.herokuapp.com",
     bucketBaseUrl: "https://jbalbino-curso-spring.s3-sa-east-1.amazonaws.com/"
 }
